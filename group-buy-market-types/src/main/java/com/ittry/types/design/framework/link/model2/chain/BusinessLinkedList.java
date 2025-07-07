9c8341bdeedc585aa7ad43acbc62a2147119e0ff
“emmm1882” <3518927016@qq.com>
2025-07-07 20:31:24 +0800

    feat:责任链抽象模板设计

---
+++ group-buy-market-types/src/main/java/com/ittry/types/design/framework/link/model2/chain/BusinessLinkedList.java
@@ -0,0 +1,28 @@
+package com.ittry.types.design.framework.link.model2.chain;
+
+import com.ittry.types.design.framework.link.model2.handler.ILogicHandler;
+
+/**
+ * @description 业务链路
+ */
+public class BusinessLinkedList<T, D, R> extends LinkedList<ILogicHandler<T, D, R>> implements ILogicHandler<T, D, R>{
+
+    public BusinessLinkedList(String name) {
+        super(name);
+    }
+
+    @Override
+    public R apply(T requestParameter, D dynamicContext) throws Exception {
+        Node<ILogicHandler<T, D, R>> current = this.first;
+        do {
+            ILogicHandler<T, D, R> item = current.item;
+            R apply = item.apply(requestParameter, dynamicContext);
+            if (null != apply) return apply;
+
+            current = current.next;
+        } while (null != current);
+
+        return null;
+    }
+
+}
